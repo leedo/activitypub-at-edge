97e638f39089fbe38aa44e45eeb6427bd670b814
Lee Aylward <lee@laylward.com>
2022-11-22 14:00:01 -0500

    rendering

--- render/post.go
+++ render/post.go
@@ -2,6 +2,7 @@ package render
 
 import (
 	"bufio"
+	"fmt"
 	"io"
 	"strings"
 
@@ -22,19 +23,26 @@ func Post(w io.Writer, p *activitypub.Person, o *activitypub.Object) {
 	} else {
 		b.Write(o.ID)
 	}
-	for _, a := range o.Attachments() {
-		if a.Type == "Document" {
-			if strings.HasPrefix(a.MediaType, "image/") {
-				b.WriteString(`<hr>`)
-				b.WriteString(`<a target="_blank" href="` + a.URL + `"><img height="200" src="` + a.URL + `"></a>`)
-			} else if strings.HasPrefix(a.MediaType, "video/") {
-				b.WriteString(`<video controls height="200">`)
-				b.WriteString(`<source src="` + a.URL + `" type="` + a.MediaType + `">`)
-				b.WriteString(`</video>`)
-			} else {
-				b.WriteString(`<a target="_blank" href="` + a.URL + `">` + a.URL + `</a>`)
+	attachments := o.Attachments()
+	if len(attachments) > 0 {
+		b.WriteString(`<table border="1" cellpadding="5"><tbody>`)
+		b.WriteString(`<tr><td colspan="` + fmt.Sprintf("%d", len(attachments)) + `">Attachments</td></tr><tr>`)
+		for _, a := range attachments {
+			b.WriteString(`<td>`)
+			if a.Type == "Document" {
+				if strings.HasPrefix(a.MediaType, "image/") {
+					b.WriteString(`<a target="_blank" href="` + a.URL + `"><img width="150" src="` + a.URL + `"></a>`)
+				} else if strings.HasPrefix(a.MediaType, "video/") {
+					b.WriteString(`<video controls width="150">`)
+					b.WriteString(`<source src="` + a.URL + `" type="` + a.MediaType + `">`)
+					b.WriteString(`</video>`)
+				} else {
+					b.WriteString(`<a target="_blank" href="` + a.URL + `">` + a.URL + `</a>`)
+				}
 			}
+			b.WriteString(`</td>`)
 		}
+		b.WriteString(`</tbody></table>`)
 	}
 	b.WriteString(`</td></tr><tr><td>`)
 	b.WriteString(`<a href="/` + string(o.ID) + `">` + o.Published() + `</a>`)
