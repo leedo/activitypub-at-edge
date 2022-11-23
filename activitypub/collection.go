5a128cc95a52f0794d7e0ad2f1eff40f9440407e
Lee Aylward <lee@laylward.com>
2022-11-22 19:27:23 -0500

    rename

--- activitypub/collection.go
+++ activitypub/collection.go
@@ -2,15 +2,15 @@ package activitypub
 
 import "github.com/valyala/fastjson"
 
-func (o *Collection) TotalCollectionItems() uint { return o.json.GetUint("totalCollectionItems") }
-func (o *Collection) First() string              { return string(o.json.GetStringBytes("first")) }
-func (o *Collection) Last() string               { return string(o.json.GetStringBytes("last")) }
-func (o *Collection) Next() string               { return string(o.json.GetStringBytes("next")) }
-func (o *Collection) Prev() string               { return string(o.json.GetStringBytes("prev")) }
+func (c *Collection) TotalCollectionItems() uint { return o.json.GetUint("totalCollectionItems") }
+func (c *Collection) First() string              { return string(o.json.GetStringBytes("first")) }
+func (c *Collection) Last() string               { return string(o.json.GetStringBytes("last")) }
+func (c *Collection) Next() string               { return string(o.json.GetStringBytes("next")) }
+func (c *Collection) Prev() string               { return string(o.json.GetStringBytes("prev")) }
 
-func (o *Collection) CollectionItems() []*CollectionItem {
+func (c *Collection) CollectionItems() []*CollectionItem {
 	items := make([]*CollectionItem, 0)
-	for _, v := range o.json.GetArray("orderedItems") {
+	for _, v := range c.json.GetArray("orderedItems") {
 		items = append(items, &CollectionItem{v, v.GetStringBytes("id")})
 	}
 	return items
