145418e997d8ec00ce3321083b0f8a08848c51a0
Lee Aylward <lee@laylward.com>
2022-11-23 23:22:42 -0800

    show summary

--- activitypub/person.go
+++ activitypub/person.go
@@ -1,9 +1,10 @@
 package activitypub
 
-func (p *Person) ID() string     { return string(p.json.GetStringBytes("id")) }
-func (p *Person) URL() string    { return string(p.json.GetStringBytes("url")) }
-func (p *Person) Outbox() string { return string(p.json.GetStringBytes("outbox")) }
-func (p *Person) Name() string   { return string(p.json.GetStringBytes("name")) }
+func (p *Person) ID() string      { return string(p.json.GetStringBytes("id")) }
+func (p *Person) URL() string     { return string(p.json.GetStringBytes("url")) }
+func (p *Person) Outbox() string  { return string(p.json.GetStringBytes("outbox")) }
+func (p *Person) Name() string    { return string(p.json.GetStringBytes("name")) }
+func (p *Person) Summary() string { return string(p.json.GetStringBytes("summary")) }
 
 func (p *Person) Icon() Image {
 	return Image{string(p.json.Get("icon").GetStringBytes("url"))}
--- render/person.go
+++ render/person.go
@@ -11,5 +11,6 @@ func PersonHeader(w io.Writer, p *activitypub.Person) {
 	b := bufio.NewWriter(w)
 	b.WriteString(`<A HREF="/` + p.URL() + `"><IMG SRC="` + p.Image().URL + `" HEIGHT="200"></A>`)
 	b.WriteString(`<H1><A HREF="/` + p.URL() + `">` + p.Name() + `</A>'s Page</H1>`)
+	b.WriteString(p.Summary())
 	b.Flush()
 }
